98d67fccf1194ff90e7b7f5560276aab2a8908ea
Milind Srivastava <milindsrivastava1997@gmail.com>
2026-09-10 16:34:08 -0400

    feat: add MetricsQL range functions

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -1345,6 +1345,30 @@ mod tests {
                     Expr::new_call(get_function("rate").unwrap(), FunctionArgs::new_args(ex))
                 })
             }),
+            ("distinct_over_time(some_metric[5m])", {
+                Expr::new_matrix_selector(
+                    Expr::from(VectorSelector::from("some_metric")),
+                    duration::MINUTE_DURATION * 5,
+                )
+                .and_then(|ex| {
+                    Expr::new_call(
+                        get_function("distinct_over_time").unwrap(),
+                        FunctionArgs::new_args(ex),
+                    )
+                })
+            }),
+            ("entropy_over_time(some_metric[5m])", {
+                Expr::new_matrix_selector(
+                    Expr::from(VectorSelector::from("some_metric")),
+                    duration::MINUTE_DURATION * 5,
+                )
+                .and_then(|ex| {
+                    Expr::new_call(
+                        get_function("entropy_over_time").unwrap(),
+                        FunctionArgs::new_args(ex),
+                    )
+                })
+            }),
             ("round(some_metric)", {
                 let ex = Expr::from(VectorSelector::from("some_metric"));
                 Expr::new_call(get_function("round").unwrap(), FunctionArgs::new_args(ex))
